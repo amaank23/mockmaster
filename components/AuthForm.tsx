892f786709d72697c65ed7af3a44443861064be1
Aman khan <amaanwaseem24@hotmail.com>
2025-12-07 06:08:58 +0500

    firebase signup and sign in logic implemented using both firebase client and firebase admin sdk

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -10,6 +10,12 @@ import Link from "next/link";
 import { toast } from "sonner";
 import FormFieldCustom from "./FormField";
 import { useRouter } from "next/navigation";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { auth } from "@/firebase/client";
+import { signIn, signUp } from "@/lib/actions/auth.action";
 
 const authFormSchema = (type: FormType) => {
   return z.object({
@@ -36,13 +42,43 @@ const AuthForm = ({ type }: { type: FormType }) => {
   });
 
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
+  async function onSubmit(values: z.infer<typeof formSchema>) {
     try {
       if (type === "sign-up") {
-        toast.success("Account created successfully. Please sign in.");
+        const { email, name, password } = values;
+        const userCredentials = await createUserWithEmailAndPassword(
+          auth,
+          email,
+          password
+        );
+        const result = await signUp({
+          email: email,
+          name: name!,
+          password: password,
+          uid: userCredentials.user.uid,
+        });
 
+        if (!result?.success) {
+          toast.error(result?.message);
+          return;
+        }
+        toast.success("Account created successfully. Please sign in");
         router.push("/sign-in");
       } else {
+        const { email, password } = values;
+        const userCredentail = await signInWithEmailAndPassword(
+          auth,
+          email,
+          password
+        );
+
+        const idToken = await userCredentail.user.getIdToken();
+        if (!idToken) {
+          toast.error("Sign in failed");
+          return;
+        } else {
+          await signIn({ email, idToken });
+        }
         toast.success("Sign in successfully.");
 
         router.push("/");
--- firebase/admin.ts
+++ firebase/admin.ts
@@ -1,7 +1,7 @@
-import { initializeApp } from "firebase-admin";
-import { cert, getApps } from "firebase-admin/app";
+import { initializeApp, cert, getApps } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
 import { getFirestore } from "firebase-admin/firestore";
+
 const initFirebaseAdmin = () => {
   const apps = getApps();
   if (!apps.length) {
