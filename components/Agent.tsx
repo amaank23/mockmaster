0caf41ff1ec8b7c23d2793b5b61af59ca13b192b
Aman khan <amaanwaseem24@hotmail.com>
2025-12-08 02:26:38 +0500

    created interview generation page

---
+++ components/Agent.tsx
@@ -0,0 +1,85 @@
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import React from "react";
+
+enum CALL_STATUS {
+  INACTIVE = "INACTIVE",
+  CONNECTING = "CONNECTING",
+  ACTIVE = "ACTIVE",
+  FINISHED = "FINISHED",
+}
+
+const Agent = ({ userName }: AgentProps) => {
+  const isSpeaking = true;
+  const callStatus = CALL_STATUS.FINISHED;
+  const messages: string[] = ["Whats your name?", "My name is john doe"];
+  const lastMessage = messages[messages.length - 1];
+  return (
+    <>
+      <div className="call-view">
+        <div className="card-interviewer">
+          <div className="avatar">
+            <Image
+              src={"/ai-avatar.png"}
+              alt="Vapi"
+              width={65}
+              height={54}
+              className="object-cover"
+            />
+            {isSpeaking && <span className="animate-speak"></span>}
+          </div>
+          <h3>AI Interviewer</h3>
+        </div>
+        <div className="card-border">
+          <div className="card-content">
+            <Image
+              src={"/user-avatar.png"}
+              alt="user avatar"
+              width={540}
+              height={540}
+              className="rounded-full object-cover size-[120px]"
+            />
+            <h3>{userName}</h3>
+          </div>
+        </div>
+      </div>
+      {messages.length > 0 && (
+        <div className="transcript-border">
+          <div className="transcript">
+            <p
+              key={lastMessage}
+              className={cn(
+                "transition-opacity duration-500 opacity-0",
+                "animate-fadeIn opacity-100"
+              )}
+            >
+              {lastMessage}
+            </p>
+          </div>
+        </div>
+      )}
+      <div className="w-full flex justify-center">
+        {callStatus !== CALL_STATUS.ACTIVE ? (
+          <button className="relative btn-call">
+            <span
+              className={cn(
+                "absolute animate-ping rounded-full opacity-75",
+                callStatus !== CALL_STATUS.CONNECTING && "hidden"
+              )}
+            />
+            <span>
+              {callStatus === CALL_STATUS.INACTIVE ||
+              callStatus === CALL_STATUS.FINISHED
+                ? "Call"
+                : ". . ."}
+            </span>
+          </button>
+        ) : (
+          <button className="btn-disconnect">END</button>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default Agent;
